da7b66b94199b38c9eff855df5944724ace820b8
Marco Franceschi <franceschi.marco@gmail.com>
2022-09-29 18:00:55 -0300

    feat: Added GitConfig types for Generator

--- sdk/autocloud_sdk/models.go
+++ sdk/autocloud_sdk/models.go
@@ -18,13 +18,30 @@ type IacCatalogFile struct {
 	FilenameVars     map[string]string `json:"fileNameVars"`
 }
 
+type IacCatalogGitConfig struct {
+	DestinationBranch string                `json:"destinationBranch"`
+	PullRequest       IacCatalogGitConfigPR `json:"pullRequest"`
+}
+
+type IacCatalogGitConfigPR struct {
+	Title                 string                      `json:"title"`
+	CommitMessageTemplate string                      `json:"commitMessageTemplate"`
+	Body                  string                      `json:"body"`
+	Variables             IacCatalogGitConfigTemplate `json:"variables"`
+}
+
+type IacCatalogGitConfigTemplate struct {
+	AuthorName  string `json:"authorName"`
+	ClusterName string `json:"clusterName"`
+}
 
 type IacCatalogInput struct {
-	Name            string           `json:"name"`
-	Author          string           `json:"author"`
-	Slug            string           `json:"slug"`
-	Description     string           `json:"description"`
-	Instructions    string           `json:"instructions"`
-	Labels          []string         `json:"labels"`
-	FileDefinitions []IacCatalogFile `json:"fileDefinitions"`
+	Name            string              `json:"name"`
+	Author          string              `json:"author"`
+	Slug            string              `json:"slug"`
+	Description     string              `json:"description"`
+	Instructions    string              `json:"instructions"`
+	Labels          []string            `json:"labels"`
+	GitConfig       IacCatalogGitConfig `json:"gitConfig"`
+	FileDefinitions []IacCatalogFile    `json:"fileDefinitions"`
 }
