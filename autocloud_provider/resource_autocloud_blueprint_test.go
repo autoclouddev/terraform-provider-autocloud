c438b9e87a2f2821d987acd15d4638a6d860b2e3
Marco Franceschi <franceschi.marco@gmail.com>
2022-10-25 16:55:26 -0300

    chore: Extend test for autocloud blueprint

--- autocloud_provider/resource_autocloud_blueprint_test.go
+++ autocloud_provider/resource_autocloud_blueprint_test.go
@@ -1,6 +1,7 @@
 package autocloud_provider
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -50,7 +51,7 @@ resource "autocloud_blueprint" "bar" {
     pull_request {
       title                   = "[AutoCloud] new static site {{siteName}} , created by {{authorName}}"
       commit_message_template = "[AutoCloud] new static site, created by {{authorName}}"
-      body                    = ""
+      body                    = "Body Example"
       variables = {
         authorName = "generic.authorName",
         siteName   = "generic.SiteName"  #autocloud_module.s3_bucket.variables["bucket_name"].id
@@ -82,6 +83,38 @@ func TestAccAutocloudBlueprint(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						"autocloud_blueprint.bar", "name", "FirstBluePrint"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "author", "enrique.enciso@autocloud.dev"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "description", "Terraform Generator for Elastic Kubernetes Service"),
+					resource.TestMatchResourceAttr(
+						"autocloud_blueprint.bar", "instructions", regexp.MustCompile("To deploy this generator, follow these simple steps")),
+					resource.TestCheckResourceAttrSet(
+						"autocloud_blueprint.bar", "labels.0"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "file.0.action", "CREATE"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "file.0.path_from_root", "some-path"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "file.0.filename_template", "eks-cluster-{{clusterName}}.tf"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "file.0.filename_vars.clusterName", "EKSGenerator.clusterName"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "git_config.0.destination_branch", "main"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "git_config.0.git_url_default", "github.com/autoclouddev/terraform-generator-test"),
+					resource.TestCheckResourceAttrSet(
+						"autocloud_blueprint.bar", "git_config.0.git_url_options.0"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "git_config.0.pull_request.0.title", "[AutoCloud] new static site {{siteName}} , created by {{authorName}}"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "git_config.0.pull_request.0.commit_message_template", "[AutoCloud] new static site, created by {{authorName}}"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "git_config.0.pull_request.0.body", "Body Example"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "git_config.0.pull_request.0.variables.authorName", "generic.authorName"),
+					resource.TestCheckResourceAttr(
+						"autocloud_blueprint.bar", "git_config.0.pull_request.0.variables.siteName", "generic.SiteName"),
 				),
 			},
 		},
