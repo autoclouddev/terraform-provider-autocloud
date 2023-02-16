1af86e54a3acb26a22ed488af44974746ca17323
Chris Koning <chris@autocloud.dev>
2023-02-16 15:43:27 -0800

    docs(examples): add public module getting started example

---
+++ examples/getting_started/providers.tf
@@ -0,0 +1,16 @@
+provider "autocloud" {
+    ###
+    # AutoCloud API Token
+    #
+    # Authorizes user to interact with AutoCloud API. These must be generated here:
+    #
+    # https://app.autocloud.io/settings/integrations/terraform-provider
+    #
+    # Value must be set eithe via environment variable:
+    #
+    # export AUTOCLOUD_TOKEN=
+    # 
+    # or via explicit configuraiton:
+
+    # token = ""
+}
