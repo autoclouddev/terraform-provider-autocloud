62485548f04768ea6db89c958ae7af6b2b0f8e64
Chris Koning <chris@autocloud.dev>
2023-06-06 12:04:49 -0700

    docs(examples): remove old examples

--- examples/file_content/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "account_num" {
-  type        = string
-  description = "Target AWS account number, mandatory"
-}
-
-variable "aws_role" {
-  description = "AWS role to assume"
-  type        = string
-}
